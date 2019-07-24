2245b4d0be3ce69ed46fd1b761d3f4ae2c819f43
Tony <test>
2019-07-24 17:17:34 +0200

    Testen...

--- src/test/java/com/hs/osna/buysomemovies/UserTest.java
+++ src/test/java/com/hs/osna/buysomemovies/UserTest.java
@@ -6,22 +6,18 @@ import org.junit.jupiter.api.Test;
 
 public class UserTest {
 
-	@Test
-	@DisplayName("Check User Complete")
-	public void checkUserComplete() {
-		User user = new User();
-		user.setUsername("Hallo");
-		Assertions.assertEquals("Hallo", user.getUsername());
-		user.setPassword("123456789");
-		Assertions.assertEquals("123456789", user.getPassword());
-		user.setCredit(100);
-		Assertions.assertEquals(100, user.getCredit());
-		boolean checkNull = user.equals(null);
-		Assertions.assertFalse(checkNull);
-		boolean identical = user.equals(user);
-		Assertions.assertTrue(identical);
-		boolean notSameUser = user.equals(new User("Test","12345678"));
-		Assertions.assertFalse(notSameUser);
-	}
+	/*
+	 * @Test
+	 * 
+	 * @DisplayName("Check User Complete") public void checkUserComplete() { User
+	 * user = new User(); user.setUsername("Hallo");
+	 * Assertions.assertEquals("Hallo", user.getUsername());
+	 * user.setPassword("123456789"); Assertions.assertEquals("123456789",
+	 * user.getPassword()); user.setCredit(100); Assertions.assertEquals(100,
+	 * user.getCredit()); boolean checkNull = user.equals(null);
+	 * Assertions.assertFalse(checkNull); boolean identical = user.equals(user);
+	 * Assertions.assertTrue(identical); boolean notSameUser = user.equals(new
+	 * User("Test","12345678")); Assertions.assertFalse(notSameUser); }
+	 */
 	
 }
